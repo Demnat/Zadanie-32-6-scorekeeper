37948498274367b51e9bd54e24c8f55724460da0
Natalia Jędrosz <n.jedrosz@gmail.com>
2019-02-14 11:10:38 +0100

    WIP

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './Components/PlayersList/PlayersList';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	shallow(<App />);
+});
+
+it('should update player score', () => {
+	const appComponent = shallow(<App />);
+	console.log(appComponent.debug());
+	const players = [
+		{
+			name: 'Kunegunda',
+			score: 5
+		}
+	];
+	appComponent.setState({ players });
+
+	const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+	
+	onScoreUpdate(0, 5);
 });
